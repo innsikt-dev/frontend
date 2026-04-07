26d6b2faa106490e69f12996c84764f07bd5a49b
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-04-07 13:03:42 +0200

    feat: add distinct style for active navigation links

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,13 +1,24 @@
+'use client'
 import Link from 'next/link'
 import { navItems } from './nav-items'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/cn'
 
 export default function Nav() {
+  const pathname = usePathname()
+  const isActive = (item: (typeof navItems)[number]) => pathname === item.path
+
   return (
     <nav>
       <ul className="flex justify-between items-end text-content gap-4">
         {navItems.map((item) => {
           return (
-            <li className="nav-link cursor-pointer" key={item.id}>
+            <li
+              className={cn('nav-link cursor-pointer px-2 rounded-lg', {
+                'border-b border-line': isActive(item),
+              })}
+              key={item.id}
+            >
               <Link className="flex items-center gap-2" href={item.path}>
                 {item.label}
               </Link>
